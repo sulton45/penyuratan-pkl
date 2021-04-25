3296de00d57272e80faef7f4b267227f8905ba4b
sulton45 <sultonibrahim00@gmail.com>
2021-04-25 15:21:02 +0700

    Update Database

--- Crud/db/database_surat.sql
+++ Crud/db/database_surat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2021 at 10:15 AM
+-- Generation Time: Apr 25, 2021 at 10:20 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
