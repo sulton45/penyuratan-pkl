3aaa1a9ee33402e021a1c3e74eee46695ec1a240
sulton45 <sultonibrahim00@gmail.com>
2021-05-21 15:12:42 +0700

    Harusnya terakhir ini

--- PUSDIKLAT/application/views/root/penelitian.php
+++ PUSDIKLAT/application/views/root/penelitian.php
@@ -46,17 +46,25 @@
 						</a>
 					</td>
 
-					<td>
-						<a href="<?= base_url("root/pdf2/") . $p['id_penelitian']; ?>" class="btn btn-sm" target="_blank">
-							<img src="<?= base_url("assets/printer.png"); ?>" width="25" data-toggle="tooltip" title="Print Surat Untuk Pemohon">
-					</td>
 
 					<td>
-						<a href="<?= base_url("root/pdf3/") . $p['id_penelitian']; ?>" class="btn btn-sm" target="_blank">
-							<img src="<?= base_url("assets/printer.png"); ?>" width="25" data-toggle="tooltip" title="Print Surat Untuk Kepala Pusat">
+						<div class="dropdown show">
+							<a class="btn btn-light dropdown-toggle" href="<?= base_url("root/pdf2/") . $p['id_penelitian']; ?>" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<img src="<?= base_url("assets/printer.png"); ?>" width="25" data-toggle="tooltip" title="Print Surat">
+							</a>
+
+							<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+								<a class="dropdown-item" href="<?= base_url("root/pdf2/") . $p['id_penelitian']; ?>" class="btn btn-sm" target="_blank">Pemohon</a>
+								<a class="dropdown-item" href="<?= base_url("root/pdf3/") . $p['id_penelitian']; ?>" class="btn btn-sm" target="_blank">Kepala Pusat</a>
+							</div>
+						</div>
 					</td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
 	</table>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
